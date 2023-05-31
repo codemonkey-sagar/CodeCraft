ce7f3cddc8eccef13050062913abdde9bf4dac37
Sagar Budhathoki <fmtsagar@gmail.com>
2023-06-01 00:57:55 +0545

    Implemented sound and updating socre when player wins or draw

--- JS/TicTacToe/script.js
+++ JS/TicTacToe/script.js
@@ -1,5 +1,21 @@
 let currentPlayer = "X";    // starting player
 
+// Player scores 
+let playerXScore = 0;
+let playerYScore = 0;
+let drawScore = 0;
+
+// Audio file for different sound 
+const clickSound = new Audio("sound/click.mp3");
+const drawSound = new Audio("sound/draw.wav");
+const winSound = new Audio("sound/win.wav");
+
+// 1. Add event listener to cell 
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => {
+  cell.addEventListener("click", handleCellClick);
+});
+
 function handleCellClick(event) {
   const selectedCell = event.target;
 
@@ -11,18 +27,42 @@ function handleCellClick(event) {
   // Set the current player's symbol to the selected cell 
   selectedCell.textContent = currentPlayer;
 
+  // Play click sound 
+  clickSound.play();
+
   // Check for win or draw condition 
   if (checkWinCondition() === currentPlayer) {
+    debugger;
     // Player win 
     console.log(`Player ${currentPlayer} wins!`);
 
     // Update scores, display game over message, etc 
+    if (currentPlayer === "X") {
+      playerXScore++;
+    } else {
+      playerYScore++;
+    }
+
+    document.querySelector(".player-1 .score").textContent = playerXScore;
+    document.querySelector(".player-2 .score").textContent = playerYScore;
+    winSound.play();
+    
+    setTimeout(() => {
+      alert(`Player ${currentPlayer} wins!`);
+      resetGame();
+    }, 100);
 
   } else if (checkDrawCondition()) {
     // Draw Condition
-    console.log("It's a draw!");
+    drawScore++;
+    document.querySelector(".draw .score").textContent = drawScore;
+    drawSound.play();
 
     // Display game over message, etc
+    setTimeout(() => {
+      alert("It's a DRAW!");
+      resetGame();
+    }, 100)
 
   } else {
     // Switch to the next player 
@@ -69,8 +109,10 @@ function checkDrawCondition() {
   return true;
 }
 
-// 1. Add event listener to cell 
-const cells = document.querySelectorAll(".cell");
-cells.forEach(cell => {
-  cell.addEventListener("click", handleCellClick);
-});
+function resetGame() {
+  // Clear the board 
+  cells.forEach(cell => {
+    cell.textContent = ""
+  })
+}
+
