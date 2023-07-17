e4738c21a05e9fcae4180fd995d08951131b66c8
Sagar Budhathoki <fmtsagar@gmail.com>
2023-07-17 22:22:35 +0545

    React: adopt-me project setup

--- React/Adopt-Me/src/App.jsx
+++ React/Adopt-Me/src/App.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
+    React.createElement("h2", {}, props.breed),
   ]);
 };
 
@@ -12,21 +15,21 @@ const App = () => {
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
-      breed: "Havanese"
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
-      breed: "Cockatiel"
+      breed: "Cockatiel",
     }),
     React.createElement(Pet, {
       name: "Doink",
       animal: "Cat",
-      breed: "Mixed"
+      breed: "Mixed",
     }),
   ]);
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
