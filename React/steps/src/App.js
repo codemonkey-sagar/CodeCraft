41009f9a6cfcd3f1386926176077d398f25251e9
Sagar Budhathoki <fmtsagar@gmail.com>
2023-10-15 23:46:50 +0545

    Steps

---
+++ React/steps/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import messages from "./data";
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step => step - 1)
+  };
+
+  function handleNext() {
+    if (step < 3) setStep(step => step + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen(isOpen => !isOpen)}>&times;</button>
+      {
+        isOpen && (
+          <div>
+            <div className="steps">
+              <div className="numbers">
+                <div className={step >= 1 ? "active" : ""}>1</div>
+                <div className={step >= 2 ? "active" : ""}>2</div>
+                <div className={step >= 3 ? "active" : ""}>3</div>
+              </div >
+
+              <p className="message">Step {step}: {messages[step - 1]}</p>
+
+              <div className="buttons">
+                <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious}>Previous</button>
+                <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>Next</button>
+              </div>
+            </div >
+          </div >
+        )
+      }
+    </>
+  )
+}
---
+++ React/steps/src/data.js
@@ -0,0 +1,7 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default messages;
