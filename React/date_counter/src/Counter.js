0161b770ffebe080e1a90c00bf129b9c44a21124
Sagar Budhathoki <fmtsagar@gmail.com>
2023-10-17 01:20:02 +0545

    update date counter

--- React/date_counter/src/Counter.js
+++ React/date_counter/src/Counter.js
@@ -7,16 +7,21 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
   return (
     <div>
       <div>
-        <button onClick={() => setStep(step => step - 1)}>-</button>
+        <input type="range" min="0" max="10" value={step} onChange={e => setStep(Number(e.target.value))} />
         <span>Step: {step}</span>
-        <button onClick={() => setStep(step => step + 1)}>+</button>
       </div>
+
       <div>
         <button onClick={() => setCount(count => count - step)}>-</button>
-        <span>Count: {count}</span>
+        <input type="text" value={count} onChange={e => setCount(Number(e.target.value))} />
         <button onClick={() => setCount(count => count + step)}>+</button>
       </div>
 
@@ -24,6 +29,11 @@ function Counter() {
         <span>{count === 0 ? "Today is " : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `}</span>
         <span>{date.toDateString()}</span>
       </p>
+
+      {(step !== 1 || count !== 0) ? <div>
+        <button onClick={handleReset}>Reset</button>
+      </div> : null}
+
     </div>
   )
 };
