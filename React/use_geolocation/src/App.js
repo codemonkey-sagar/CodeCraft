03da2a75e5ae40f1908438561e6ce7a68fe6cbbf
Sagar Budhathoki <fmtsagar@gmail.com>
2023-10-30 01:45:35 +0545

    useGeolocaton project: completed

---
+++ React/use_geolocation/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useGeolocation } from "./useGeolocation";
+
+export default function App() {
+  const { isLoading, position: { lat, lng }, error, getPosition } = useGeolocation();
+
+  const [countClicks, setCountClicks] = useState(0);
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
+  return (
+    <div className="App">
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
