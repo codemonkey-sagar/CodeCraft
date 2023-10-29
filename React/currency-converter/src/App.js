48b07b55a6788110594eeadc6da08f056dba5308
Sagar Budhathoki <fmtsagar@gmail.com>
2023-10-29 14:14:51 +0545

    currency-converter

---
+++ React/currency-converter/src/App.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function convert() {
+      setIsLoading(true);
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`);
+      const data = await res.json();
+      setConverted(data.rates[toCur]);
+      setIsLoading(false);
+    }
+    if (fromCur === toCur) return setConverted(amount);
+    convert();
+  }, [amount, fromCur, toCur]);
+
+  return (
+    <div className="App">
+      <input type="text" value={amount} onChange={e => setAmount(Number(e.target.value))} disabled={isLoading} />
+
+      <select value={fromCur} onChange={e => setFromCur(e.target.value)} disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <select value={toCur} onChange={e => setToCur(e.target.value)} disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <p>
+        {converted} {toCur}
+      </p>
+    </div>
+  );
+}
